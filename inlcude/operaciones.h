509a74adcb3c9c3f126bf6d817942d836caad4d4
Nabetse28 <esteban_hv.09@hotmail.com>
2019-09-11 01:12:28 -0600

    Tutorial 5 fixes

--- inlcude/operaciones.h
+++ inlcude/operaciones.h
@@ -1,7 +1,10 @@
-#include <stdio.h>
+#ifndef _OPERACIONES_H
+#define _OPERACIONES_H
 
 float sum(float a, float b);
 float sub(float a, float b);
 float mul(float a, float b);
 float div(float a, float b);
 float sqr(float a);
+
+#endif
